13f800ace080a1b21d2bd69cd658ad7649d82aba
Lucas Correia <lucasdevproject@gmail.com>
2024-10-31 21:37:10 -0300

    .

--- app_guia/App.js
+++ app_guia/App.js
@@ -1,11 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TabNavigations from './App/Navigations/TabNavigations';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <TabNavigations/>
+      </NavigationContainer>
     </View>
   );
 }
--- app_guia/App/Navigations/TabNavigations.js
+++ app_guia/App/Navigations/TabNavigations.js
@@ -2,9 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function TabNavigations() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View>
-      <Text>TabNavigations</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Fav" component={Fav} />
+    </Tab.Navigator>
   )
 }
